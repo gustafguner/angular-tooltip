a4b77185f5bf6c301e9b5e18161bbee22898411a
Gustaf Gunér <gustaf@guner.se>
2018-08-17 15:18:27 +0200

    basic hover show/hide

--- src/app/tooltip-test.directive.ts
+++ src/app/tooltip-test.directive.ts
@@ -11,12 +11,21 @@ export class TooltipTestDirective {
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.renderer.addClass(this.el.nativeElement, 'at-container');
     console.log(this.tooltipText);
     this.tooltip = this.renderer.createElement('div');
-    this.renderer.addClass(this.tooltip, 'angular-tooltip');
+    this.renderer.addClass(this.tooltip, 'at-tooltip');
     const tooltipContent = this.renderer.createText(this.tooltipText);
     this.renderer.appendChild(this.tooltip, tooltipContent);
     this.renderer.appendChild(this.el.nativeElement, this.tooltip);
+  }
+
+  @HostListener('mouseenter') onmouseenter() {
+    this.renderer.addClass(this.el.nativeElement, 'at-active');
+  }
+
+  @HostListener('mouseleave') onmouseleave() {
+    this.renderer.removeClass(this.el.nativeElement, 'at-active');
   }
 
 }
