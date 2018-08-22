890f7ba29c6e8581a463482dddec261ca8dfe383
Gustaf Gunér <gustaf@guner.se>
2018-08-22 19:18:11 +0200

    Design

--- src/app/tooltip-test.directive.ts
+++ src/app/tooltip-test.directive.ts
@@ -7,40 +7,29 @@ export class TooltipTestDirective {
   @Input('tooltip-text') tooltipText: string;
   @Input('tooltip-direction') tooltipDirection: string;
 
-  tooltip: ElementRef;
+  atTooltip: ElementRef;
   atWrapper: ElementRef;
+  atTrigger: ElementRef;
 
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
     this.atWrapper = this.renderer.createElement('at-wrapper');
-    let atTrigger = this.renderer.createElement('at-trigger');
+    this.atTrigger = this.renderer.createElement('at-trigger');
+    this.renderer.appendChild(this.atWrapper, this.atTrigger);
 
-    const parentNode = this.el.nativeElement.parentNode;
-    
-    //this.renderer.appendChild(atTrigger, this.el.nativeElement);
-    //this.renderer.addClass(this.el.nativeElement, 'at-trigger');
-    this.renderer.appendChild(this.atWrapper, atTrigger);
-
-    // Tooltip
-    this.tooltip = this.renderer.createElement('at-tooltip');
+    this.atTooltip = this.renderer.createElement('at-tooltip');
     const tooltipContent = this.renderer.createText(this.tooltipText);
-    this.renderer.appendChild(this.tooltip, tooltipContent);
+    this.renderer.appendChild(this.atTooltip, tooltipContent);
     if (/^(top|right|bottom|left)$/.test(this.tooltipDirection)) {
-      this.renderer.addClass(this.tooltip, this.tooltipDirection);
+      this.renderer.addClass(this.atTooltip, this.tooltipDirection);
     } else {
-      this.renderer.addClass(this.tooltip, 'bottom');
+      this.renderer.addClass(this.atTooltip, 'bottom');
     }
-    // ***
-    this.renderer.appendChild(this.atWrapper, this.tooltip);
 
-    console.log(parentNode);
-    console.log(this.atWrapper);
-    console.log(this.el.nativeElement);
-
-    this.renderer.insertBefore(parentNode, this.atWrapper, this.el.nativeElement);
-    this.renderer.appendChild(atTrigger, this.el.nativeElement);
-
+    this.renderer.appendChild(this.atWrapper, this.atTooltip);
+    this.renderer.insertBefore(this.el.nativeElement.parentNode, this.atWrapper, this.el.nativeElement);
+    this.renderer.appendChild(this.atTrigger, this.el.nativeElement);
   }
 
   @HostListener('mouseenter') onmouseenter() {
