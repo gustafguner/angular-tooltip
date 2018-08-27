00e2611572533c3eb583d21040cfd2b99b2b4bfc
Gustaf Gunér <gustaf@guner.se>
2018-08-27 20:01:20 +0200

    Lib Component ready

--- projects/angular-tooltip/src/lib/angular-tooltip.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AngularTooltipDirective } from './angular-tooltip.directive';
-
-describe('AngularTooltipDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AngularTooltipDirective();
-    expect(directive).toBeTruthy();
-  });
-});
